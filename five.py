89739cf8769db828fe1311249a753d6d4f34a26b
MAMAMAisused <vin_mlc@126.com>
2019-01-28 22:33:17 +0800

    black player lef-right movable

--- five.py
+++ five.py
@@ -37,20 +37,25 @@ def load_image(file):
     return surface.convert()
 
 class Player_black(pygame.sprite.Sprite):
-    pos = position(0,0)
-    def __init__(self,img):
+    images = []    
+    def __init__(self):
         pygame.sprite.Sprite.__init__(self, self.containers)
-        self.rect = self.image.get_rect(20,20)
-        self.image = load_image(img)
+        self.image = self.images[0]
+        self.rect = pygame.Rect(240,490,20,20)
+        self.pos = position(0,0)
+        self.speed = MOVESTEP   
+        self.origtop = self.rect.top 
+        print(self.rect)
+        self.bounce = 24   
 
     def move(self, direction):
-        if direction: self.facing = direction
         self.rect.move_ip(direction*self.speed, 0)
         self.rect = self.rect.clamp(SCREENRECT)
-        self.rect.top = self.origtop - (self.rect.left//self.bounce%2)
+        self.rect.top = (self.origtop - (self.rect.left//self.bounce%2))
 
-def main():
+def main():    
     pygame.init()
+    clock = pygame.time.Clock()
     # Set the display mode
     winstyle = 0  # |FULLSCREEN
     bestdepth = pygame.display.mode_ok(SCREENRECT.size, winstyle, 32)
@@ -62,6 +67,35 @@ def main():
     #background.blit(bgdtile, (0, 0))
     screen.blit(background, (0,0))
     pygame.display.flip()
+
+    blacks = pygame.sprite.Group()
+    blues = pygame.sprite.Group()
+    all = pygame.sprite.RenderUpdates()
+    #要先加入containers不然会说self没有container，不知道为什么
+    Player_black.containers = all
+    Player_black.images = [load_image("black.png")]
+
+    bplay = Player_black()
+
+    while True:
+        all.clear(screen, background)
+
+        #get input
+        for event in pygame.event.get():
+            if event.type == QUIT or \
+                (event.type == KEYDOWN and event.key == K_ESCAPE):
+                    return
+            keystate = pygame.key.get_pressed()
+            if keystate[K_RIGHT] == 1:
+                bplay.move(1) 
+            if keystate[K_LEFT] == 1:
+                bplay.move(-1)        
+        clock.tick(40)
+        all.update()
+        #draw the scene
+        dirty = all.draw(screen)
+        pygame.display.flip()
+                
 
 #call the "main" function if running this script
 if __name__ == '__main__': main()
