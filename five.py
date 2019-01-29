33d703b81731fc17ec86a19bc1d69269b3f3b7f0
MAMAMAisused <vin_mlc@126.com>
2019-01-29 23:01:19 +0800

    值的传送存在问题，需要解决。class pos, initpos = pos,不能完成值的传送，只是建立了索引而已，initpos指向pos的内存

--- five.py
+++ five.py
@@ -13,15 +13,14 @@ LEFTBOUND = (50,50)
 RIGHTBOUND = (500,500)
 SCREENRECT = Rect(0, 0, 550, 550)
 PLAYERSIZE = 20
-INIT_VIR_POS = (9,4)#line 10,row 5
+INIT_VIR_POS = (9,0)#line 10,row 0
 main_dir = os.path.split(os.path.abspath(__file__))[0]
-gamedata[INIT_VIR_POS[0]][INIT_VIR_POS[1]] = 1
 print(gamedata)
 
 class chess(Enum):
-    none = 0
+    none = -1
     black = 1
-    white = 2
+    blue = 0
 
 class position:
     x = 0
@@ -39,13 +38,19 @@ def load_image(file):
     return surface.convert()
 
 class Player(pygame.sprite.Sprite):
-    images = []    
+    images = []  
+    pos = position(0,0)
     def __init__(self):
         pygame.sprite.Sprite.__init__(self, self.containers)
-        self.image = self.images[0]
-        self.rect = pygame.Rect(RIGHTBOUND[0]/2 - PLAYERSIZE/2,RIGHTBOUND[0]-PLAYERSIZE/2,20,20)
+        #self.image = self.images[0]
+        self.rect = pygame.Rect(MOVESTEP - PLAYERSIZE/2,RIGHTBOUND[0]-PLAYERSIZE/2,20,20)
+        self.init_rect = self.rect
         #virtual position
-        self.pos = position(INIT_VIR_POS[0],INIT_VIR_POS[1])
+        self.pos.x = INIT_VIR_POS[0]
+        self.pos.y = INIT_VIR_POS[1]
+        self.init_pos = self.pos
+        self.pixelpos = position(MOVESTEP - PLAYERSIZE/2,RIGHTBOUND[0]-PLAYERSIZE/2)
+        self.init_pixelpos = self.pixelpos
         self.speed = MOVESTEP   
         self.origtop = self.rect.top 
         #print(self.rect)
@@ -54,19 +59,45 @@ class Player(pygame.sprite.Sprite):
     def move(self, direction):
         if direction<2:
             self.rect.move_ip(direction*self.speed, 0)
+            self.pixelpos.x += direction*self.speed
+            self.pos.y += direction
         else:
             self.rect.move_ip(0,(direction-3)*self.speed)#2--up,4--down
+            self.pixelpos.y += (direction-3)*self.speed
+            self.pos.x += (direction-3)
+    def resetpos(self):
+        self.rect = self.init_rect
+        self.pos = self.init_pos
+        self.pixelpos = self.init_pixelpos
+        print(self.pos.x,self.pos.y) 
         #dont let player out of the screen
         #self.rect = self.rect.clamp(SCREENRECT)
         #self.rect.top = (self.origtop - (self.rect.left//self.bounce%2))
-    def chessdone(self):
-        pass
+    
 
-class black_player(Player):
+class chess_player(Player):
+    image = pygame.surface
     def __init__(self):
+        Player.__init__(self)
         self.role = chess.black
     def init_img(self,imgs=[]):
         Player.images = imgs
+        self.image = imgs[0]
+    def switchplayer(self):
+        if(self.role == chess.black):
+            self.role = chess.blue
+            self.image = Player.images[1]
+        elif(self.role == chess.blue):
+            self.role = chess.black
+            self.image = Player.images[0]
+        Player.resetpos(self)
+    def chessdone(self):
+        if(self.role == chess.black):
+            gamedata[Player.pos.x][Player.pos.y] = 1
+        else:
+            gamedata[Player.pos.x][Player.pos.y] = -1
+        print(gamedata)
+
 
 def main():    
     pygame.init()
@@ -75,23 +106,19 @@ def main():
     winstyle = 0  # |FULLSCREEN
     bestdepth = pygame.display.mode_ok(SCREENRECT.size, winstyle, 32)
     screen = pygame.display.set_mode(SCREENRECT.size, winstyle, bestdepth)
-    #create the background, tile the bgd image
-     
+    #create the background, tile the bgd image    
     background = load_image('background.gif')
     #for x in range(0, SCREENRECT.width, bgdtile.get_width()):
     #background.blit(bgdtile, (0, 0))
     screen.blit(background, (0,0))
     pygame.display.flip()
 
-    blacks = pygame.sprite.Group()
-    blues = pygame.sprite.Group()
     all = pygame.sprite.RenderUpdates()
     #要先加入containers不然会说self没有container，不知道为什么
-    black_player.containers = all
-    #black_player.init_img([load_image("black.png")])
+    chess_player.containers = all
 
-    bplay = black_player()
-    bplay.init_img([load_image("black.png")])
+    pblack= chess_player()
+    pblack.init_img([load_image("black.png"),load_image("blue.png")])
 
     while True:
         all.clear(screen, background)
@@ -103,17 +130,21 @@ def main():
                     return
             keystate = pygame.key.get_pressed()
             if keystate[K_RIGHT] == 1:
-                bplay.move(1) 
+                pblack.move(1) 
             if keystate[K_LEFT] == 1:
-                bplay.move(-1)
+                pblack.move(-1)
             if keystate[K_UP] == 1:
-                bplay.move(2)
+                pblack.move(2)
             if keystate[K_DOWN] == 1:
-                bplay.move(4)        
+                pblack.move(4) 
+            if keystate[K_SPACE] == 1:
+                background.blit(pblack.image,(pblack.pixelpos.x,pblack.pixelpos.y))
+                pblack.chessdone()
+                pblack.switchplayer()       
         clock.tick(40)
         all.update()
         #draw the scene
-        dirty = all.draw(screen)
+        all.draw(screen)
         pygame.display.flip()
                 
 
